cfbd27a51edf3c396a2f4a8d85458907f9842d50
Thomas Vernouillet <thomasvernouillet@Host-004.lan>
2025-09-14 13:13:08 +0200

    Fix: Corriger les noms incohérents du plugin et de l'accessoire
    - Mise à jour du nom du plugin vers 'homebridge-blinds-zigbee-mqtt-relay'
    - Mise à jour du nom de l'accessoire vers 'BlindsZigbeeMqttRelay'
    - Correction du pluginAlias dans config.schema.json
    - Version bump vers 1.1.4

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 "use strict";
 var accessory_1 = require("./accessory");
-var PLUGIN_NAME = 'homebridge-smart-blinds';
-var ACCESSORY_NAME = 'SmartBlinds';
+var PLUGIN_NAME = 'homebridge-blinds-zigbee-mqtt-relay';
+var ACCESSORY_NAME = 'BlindsZigbeeMqttRelay';
 module.exports = function (api) {
     // Forcer le type pour contourner l'erreur TypeScript
     var SmartBlindsConstructor = accessory_1.SmartBlindsAccessory;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { API, Logging, AccessoryConfig } from 'homebridge';
 import { SmartBlindsAccessory } from './accessory';
 
-const PLUGIN_NAME = 'homebridge-smart-blinds';
-const ACCESSORY_NAME = 'SmartBlinds';
+const PLUGIN_NAME = 'homebridge-blinds-zigbee-mqtt-relay';
+const ACCESSORY_NAME = 'BlindsZigbeeMqttRelay';
 
 export = (api: API) => {
   // Forcer le type pour contourner l'erreur TypeScript
